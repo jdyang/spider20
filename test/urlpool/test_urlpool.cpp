b82178f38d4d3322a0f5efd7ec3285bb4b337e84
jdyang <jdyangchina@gmail.com>
2011-10-27 21:10:41 +0800

    rm untest code

--- test/urlpool/test_urlpool.cpp
+++ test/urlpool/test_urlpool.cpp
@@ -16,36 +16,9 @@ void end_handler(int sig)
 void* select_thread(void* arg)
 {
 	UrlPool* pool = (UrlPool*)arg;
-	int cpq_url_num, coq_url_num, ipq_url_num, ioq_url_num, sq_url_num, cate_select_num, item_select_num;
-
+	
 	while(true)
 	{
-		cpq_url_num = rand()%100;
-		coq_url_num = rand()%100;
-		ipq_url_num = rand()%100;
-		ioq_url_num = rand()%100;
-		sq_url_num = rand()%100;
-		cate_select_num = rand()%100;
-		item_select_num = rand()%100;
-		
-		spider_statis->set_domain_cate_select_num("xxx.com", cate_select_num);
-		spider_statis->set_domain_item_select_num("xxx.com", item_select_num);
-		spider_statis->set_cpq_url_num(cpq_url_num);
-		spider_statis->set_ipq_url_num(ipq_url_num);
-		spider_statis->set_coq_url_num(coq_url_num);
-		spider_statis->set_ioq_url_num(ioq_url_num);
-		spider_statis->set_sq_url_num(sq_url_num);
-
-		cout<<time(NULL)<<" select_thread [domain=xxx.com] ";
-		cout<<"cpq="<<spider_statis->get_cpq_url_num()<<" ";
-		cout<<"ipq="<<spider_statis->get_ipq_url_num()<<" ";
-		cout<<"coq="<<spider_statis->get_coq_url_num()<<" ";
-		cout<<"ioq="<<spider_statis->get_ioq_url_num()<<" ";
-		cout<<"sq="<<spider_statis->get_sq_url_num()<<" ";
-		cout<<"cate_sel="<<spider_statis->get_domain_cate_select_num("xxx.com")<<" ";
-		cout<<"item_sel="<<spider_statis->get_domain_item_select_num("xxx.com")<<" ";
-		cout<<"cate_done="<<spider_statis->get_domain_cate_done_num("xxx.com")<<" ";
-		cout<<"item_done="<<spider_statis->get_domain_item_done_num("xxx.com")<<endl;
 		
 		usleep(100000);
 
@@ -57,16 +30,6 @@ void* work_thread(void* arg)
 
 	while(true)
 	{
-		if(rand()%2 == 0)
-			spider_statis->update_domain_cate_done("xxx.com");
-		else
-		{
-			spider_statis->update_domain_item_done("xxx.com");
-		}
-
-		cout<<time(NULL)<<" work_thread: [domain=xxx.com] ";
-		cout<<"cate_done="<<spider_statis->get_domain_cate_done_num("xxx.com")<<" ";
-		cout<<"item_done="<<spider_statis->get_domain_item_done_num("xxx.com")<<endl;
 		usleep(300000);
 	}
 	return NULL;
