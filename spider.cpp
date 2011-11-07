ea8879d29894032cfb7e52559f18ac2720e74b03
jdyang <jdyangchina@gmail.com>
2011-11-07 16:33:54 +0800

    m

--- spider.cpp
+++ spider.cpp
@@ -418,7 +418,7 @@ int CSpider::select_url()
 	
 	vector<string> domain_p;
 	vector<string> domain_o;
-	map<string, vector<UrlInfo> > select_map;
+	map<string, vector<UrlInfo>* > select_map;
 	vector<vector<UrlInfo>* > url_array;
 	map<string, DomainAttr>::iterator domain_it;
 	int i = -1;
@@ -447,7 +447,7 @@ int CSpider::select_url()
 	mp_ipq->get_url_mutex().lock();
 	for (it = tmp_que.begin(); it != tmp_que.end(); ++it){
 		(*it).type = 3;
-		select_map[(*it).domain].push_back(*it);
+		select_map[(*it).domain]->push_back(*it);
 	}
 	tmp_que.clear();
 	mp_cpq->get_url_mutex().unlock();
@@ -456,7 +456,7 @@ int CSpider::select_url()
 	mp_ipq->get_url_mutex().lock();
 	for (it = tmp_que1.begin(); it != tmp_que1.end(); ++it){
 		(*it).type = 2;
-		select_map[(*it).domain].push_back(*it);
+		select_map[(*it).domain]->push_back(*it);
 	}
 	tmp_que1.clear();
 	mp_ipq->get_url_mutex().unlock();
@@ -465,7 +465,7 @@ int CSpider::select_url()
 	mp_ioq->get_url_mutex().lock();
 	for (it = tmp_que2.begin(); it != tmp_que2.end(); ++it){
 		(*it).type = 0;
-		select_map[(*it).domain].push_back(*it);
+		select_map[(*it).domain]->push_back(*it);
 	}
 	tmp_que2.clear();
 	mp_ioq->get_url_mutex().unlock();
@@ -474,7 +474,7 @@ int CSpider::select_url()
 	mp_coq->get_url_mutex().lock();
 	for (it = tmp_que3.begin(); it != tmp_que3.end(); ++it){
 		(*it).type = 1;
-		select_map[(*it).domain].push_back(*it);
+		select_map[(*it).domain]->push_back(*it);
 	}	
 	tmp_que3.clear();
 	mp_coq->get_url_mutex().unlock();
